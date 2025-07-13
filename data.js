7e0c938215586ab10b1a3bfbb7a0f431c90c6a9e
PCPCHAT <Tech@protectioncivile-chatillon.org>
2025-07-13 11:03:30 +0200

    Update data.js

--- data.js
+++ data.js
@@ -30,5 +30,9 @@ const inventory = {
     { name: "Boite OPTC", expected: 1 },
     { name: "Bandes Cohésives", expected: 2 }
   ]
-
+"Poche N°8": [
+    { name: "Boite de pansement", expected: 2 },
+    { name: "Boite OPTC", expected: 1 },
+    { name: "Bandes Cohésives", expected: 2 }
+  ]
 };
