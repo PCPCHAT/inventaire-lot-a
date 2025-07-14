6415fb218629dd0497585d698f21838f06ef03da
PCPCHAT <Tech@protectioncivile-chatillon.org>
2025-07-14 16:23:50 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const inventory = {
   "Sac POM": {
-    "Poche 1": [
+    "Poche N°1": [
       { name: "BAVU Adulte + masque insufflateur", expected: 1 },
       { name: "Canule oropharyngée de Guedel T0/1/2/3/4", expected: 5 },
       { name: "Lampe frontale", expected: 2 }
     ],
-    "Poche 2": [
+    "Poche N°2": [
        { name: "Garrot", expected: 2 },
        { name: "Pansement israélien", expected: 1 },
        { name: "CHU", expected: 1 }
@@ -44,8 +44,39 @@ const inventory = {
     { name: "Tensiometre + brassards + stéthoscope", expected: 1 },
     { name: "Oxymetre de pouls", expected: 20 },
     { name: "thermometre tympanique ", expected: 20 },
+    { name: "Lecteur de Glycémie", expected: 20 },
+    { name: "Boite de bandelettes", expected: 20 }
+  ],
+"Poche N°11": [
+    { name: "Haricot", expected: 2 },
+    { name: "Ciseaux JESCO", expected: 1 },
+    { name: "Autopiqueurs", expected: 20 },
+    { name: "Sparadrap", expected: 2 },
+    { name: "Pince à écharde", expected: 2 }
+    ],
+  "Poche N°12": [
+    { name: "Poche de froid", expected: 15 },
+  ],
+    "Poche N°13": [
+    { name: "Tensiometre + brassards + stéthoscope", expected: 1 },
+    { name: "Oxymetre de pouls", expected: 20 },
+    { name: "thermometre tympanique ", expected: 20 },
     { name: "Lecteur de Glycémie", expected: 20 }
-  ]
+  ],
+    "Poche N°14": [
+    { name: "Serviettes", expected: 20 },
+    { name: "Gobelets", expected: 10 },
+    { name: "Touillette ", expected: 10 },
+    { name: "Bouchon d'oreille", expected: 10 }
+  ],
+ "Poche N°15": [
+    { name: "Lunettes de protection", expected: 5 },
+    { name: "Masque FFP2", expected: 10 },
+    { name: "Masque chirurgical", expected: 10 },
+    { name: "Touillette ", expected: 10 },
+    { name: "Bouchon d'oreille", expected: 10 }
+  ],
+    
   },
   "Sac de soin": {
     "Poche 1": [
