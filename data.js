4f95f706799ad4f6b55738ba8e80859a8195cb33
PCPCHAT <Tech@protectioncivile-chatillon.org>
2025-07-14 15:53:56 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,48 +1,37 @@
 const inventory = {
-  "Poche N°1": [
-    { name: "BAVU Adulte + masque insufflateur", expected: 1 },
-    { name: "Canule oropharyngée de Guedel T0/1/2/3/4", expected: 5 },
-    { name: "Lampe frontale", expected: 2 }
-  ],
-  "Poche N°2": [
-    { name: "Garrot", expected: 2 },
-    { name: "Pansement israélien", expected: 1 },
-    { name: "CHU", expected: 1 }
-  ],
-  "Poche N°3": [
-    { name: "BAVU pédiatrique + masque insufflateur", expected: 1 },
-    { name: "Stop-vide ou raccord biconique", expected: 2 }
-  ],
-  "Poche N°4": [
-    { name: "MHC Adulte", expected: 4 },
-    { name: "MMC Adulte", expected: 4 },
-    { name: "Lunette à oxygène", expected: 3 }
-  ],
-  "Poche N°5": [
-    { name: "HC Pédiatrique", expected: 3 },
-    { name: "MMC Pédiatrique", expected: 3 }
-  ],
-  "Poche N°6": [
-    { name: "Couverture de survie", expected: 15 },
-    { name: "VOMIX", expected: 20 }
-  ],
-  "Poche N°7": [
-    { name: "Boite de pansement", expected: 2 },
-    { name: "Boite OPTC", expected: 1 },
-    { name: "Bandes Cohésives", expected: 2 }
-  ],
-  "Poche N°8": [
-    { name: "Bandes Grande taille", expected: 10 },
-    { name: "Bandes Petite taille", expected: 10 }
-  ],
-  "Poche N°9": [
-    { name: "compresses", expected: 30 },
-    { name: "Serum Physiologique", expected: 20 }
-  ],
-"Poche N°10": [
-    { name: "Tensiometre + brassards + stéthoscope", expected: 1 },
-    { name: "Oxymetre de pouls", expected: 20 },
-    { name: "thermometre tympanique ", expected: 20 },
-    { name: "Lecteur de Glycémie", expected: 20 }
-  ]
+  "Sac POM": {
+    "Poche 1": [
+      { name: "BAVU Adulte + masque insufflateur", expected: 1 },
+      { name: "Canule oropharyngée de Guedel", expected: 5 }
+    ],
+    "Poche 2": [
+      { name: "Garrot", expected: 2 },
+      { name: "Pansement israélien", expected: 1 }
+    ]
+  },
+  "Sac de soin": {
+    "Poche 1": [
+      { name: "Gants stériles", expected: 10 },
+      { name: "Désinfectant", expected: 1 }
+    ]
+  },
+  "Sac d’O2": {
+    "Poche 1": [
+      { name: "Lunettes à oxygène", expected: 2 },
+      { name: "MHC Adulte", expected: 2 }
+    ]
+  },
+  "Matériel d’immobilisation": {
+    "Poche 1": [
+      { name: "Écharpe triangulaire", expected: 2 },
+      { name: "Attelle", expected: 2 }
+    ]
+  },
+  "Réserve": {
+    "Poche 1": [
+      { name: "VOMIX", expected: 20 },
+      { name: "Couvertures de survie", expected: 5 }
+    ]
+  }
 };
+
